c5d1a2b02dcfb189fa3d6f5aae62e90f1fee3bf1
宋祥 <790292520@qq.com>
2017-04-10 14:47:01 +0800

    配置

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,10 +3,26 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=qrqy',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
+            'tablePrefix' => 'qrqy_'
+        ],
+        'formatter' => [
+            'dateFormat' => 'php:Y-m-d',
+            'datetimeFormat' => 'php:Y-m-d H:i:s',
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            //'suffix'=>'.html',
+            'rules'=>[
+                '<controller:\w+>/<id:\d+>'=>'<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+                '<controller:\w+>/<action:\w+>.html'=>'<controller>/<action>',
+            ],
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
@@ -15,6 +31,35 @@ return [
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
             'useFileTransport' => true,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.qq.com',  //每种邮箱的host配置不一样
+                'username' => '790292520@qq.com',
+                'password' => 'gtyknjtpygribfgg',
+                'port' => '465',
+                'encryption' => 'ssl',
+
+            ],
+            'messageConfig'=>[
+                'charset'=>'UTF-8',
+                'from'=>['790292520@qq.com'=>'清净世界']
+            ],
+        ],
+        'assetManager' => [
+            'bundles' => [
+                'nezhelskoy\highlight\HighlightAsset' => [
+                    //'selector' => '.is-highlighted',
+                    'options' => [
+                        //'classPrefix' => 'custom-',
+                        //'useBR' => true,
+                    ],
+                    'css' => ['dist/styles/monokai-sublime.css'],
+                ],
+            ]
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'defaultRoles' => ['Guest'],
         ],
     ],
 ];
